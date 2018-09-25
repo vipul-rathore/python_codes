5a214731bc0af87b34f5de36de76f6a9d397d3be
vipulbanna <vipul1920.vr@gmail.com>
2018-09-25 18:54:58 +0530

    first commit

---
+++ 1p.py
@@ -0,0 +1,10 @@
+x=int(input("enter the int no.:")
+y=float(input("enter the float no.:")
+print("which operation you want to perform")
+
+if(add)
+print("additon is" x+y;)
+else(sub)
+print("sub is" x-y;)
+
+
---
+++ home.py
@@ -0,0 +1,3 @@
+name = input("enter your name:")
+print("you are a rajput",name)
+
